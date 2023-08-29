8342150155dde374159cc1bcb0ef3261c18be78c
DaiMika <maxdietunnie@gmail.com>
2023-08-28 22:33:38 -0700

    6. Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
  *pop_listint - deletes the head node of a linked list
  *@head: head of the list
  *
- *Return: the head node's data
+ *Return: the deleted node data
  */
 
 int pop_listint(listint_t **head)
